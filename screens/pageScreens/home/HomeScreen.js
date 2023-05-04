cf2ed5e510a48d6cc7181dc28f0c3cca8ff15502
MiqayelChaloyan <miqochaloyan@gmail.com>
2023-05-04 15:30:32 +0400

    ..

--- screens/pageScreens/home/HomeScreen.js
+++ screens/pageScreens/home/HomeScreen.js
@@ -1,18 +1,32 @@
-import * as React from 'react';
+import {useContext, useEffect, useState} from 'react';
 import {View, Text, Button} from 'react-native';
 import List from '../../../components/list/List';
+import GlobalContext from '../../../context/GlobalContext';
 
 const HomeScreen = ({navigation}) => {
+  const {todos} = useContext(GlobalContext);
+  const [complete, setisComplete] = useState(0);
+
+  useEffect(() => {
+    const result = todos.filter(item => item.isComplete);
+    setisComplete(result);
+  }, [todos]);
+
   return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        marginTop: 25,
-      }}>
-      <List navigation={navigation} />
-    </View>
+    <>
+      <Text style={{textAlign: 'center', margin: 15, fontSize: 14}}>
+        Some of the tasks have been completed {complete.length}/{todos.length}
+      </Text>
+      <View
+        style={{
+          flex: 1,
+          alignItems: 'center',
+          justifyContent: 'center',
+          marginTop: 25,
+        }}>
+        <List navigation={navigation} />
+      </View>
+    </>
   );
 };
 
